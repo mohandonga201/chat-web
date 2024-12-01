9303f460272b59db6d75a89e3253828283967e60
mohandonga201 <152882574+mohandonga201@users.noreply.github.com>
2024-12-01 08:01:14 +0530

    app.js

--- app.js
+++ app.js
@@ -3,18 +3,34 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const io = require('socket.io')(server);
+const socketClient = require('socket.io-client');
 
+// Connect to Server B (use Server B's IP or hostname here)
+const socketB = socketClient('http://server-b:3000');  // Docker container or server name
+
+// Serve the chat HTML file
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// Relay messages from Server B to connected clients on Server A
+socketB.on('chat message', (msg) => {
+  io.emit('chat message', msg);  // Broadcast to clients connected to Server A
+});
+
+// Handle incoming client messages and forward to Server B
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    console.log('message from client on Server A: ' + msg);
+    socketB.emit('chat message', msg);  // Send message to Server B
+  });
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log('Server A listening on *:3000');
 });
